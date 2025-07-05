0754a7cff256b6a12be532c3c3712d94a9d722a1
celikerde <celikerde@itu.edu.tr>
2025-07-05 21:50:56 +0300

    Refactor index.php to include view components and remove hardcoded book data; add index.view.php for home page layout

--- index.php
+++ index.php
@@ -1,65 +1,72 @@
 <?php
 
-    $books= [
-        [
-            "name" => "The Fountainhead",
-            "author" => "Ayn Rand",
-            "releaseYear" => 1943,
-            "purchaseUrl" => "https://www.amazon.com/The-Fountainhead-Novel-Ayn-Rand/dp/0743273567"
-        ],
-        [
-            "name" => "Atlas Shrugged",
-            "author" => "Ayn Rand",
-            "releaseYear" => 1957,
-            "purchaseUrl" => "https://www.amazon.com/Atlas-Shrugged-Novel-Ayn-Rand/dp/045116392X"
-        ],
-        [
-            "name" => "The Virtue of Selfishness",
-            "author" => "Ayn Rand",
-            "releaseYear" => 1961,
-            "purchaseUrl" => "https://www.amazon.com/Virtue-Selfishness-Essays-Ayn-Rand/dp/045116392X"
-        ],
-        [
-            "name" => "Farewell to Arms",
-            "author" => "Ernest Hemingway",
-            "releaseYear" => 1929,
-            "purchaseUrl" => "https://www.amazon.com/Farewell-Arms-Novel-Ernest-Hemingway/dp/0684830195"
-        ],
-        [
-            "name" => "The Old Man and the Sea",
-            "author" => "Ernest Hemingway",
-            "releaseYear" => 1952,
-            "purchaseUrl" => "https://www.amazon.com/Old-Man-Sea-Classic-Literature/dp/0684801063"
-        ],
-        [
-            "name" => "For Whom the Bell Tolls",
-            "author" => "Ernest Hemingway",
-            "releaseYear" => 1940,
-            "purchaseUrl" => "https://www.amazon.com/For-Whom-Bell-Tolls-Hemingway/dp/0684830195"
-        ],
-        [
-            "name" => "Karamazov Brothers",
-            "author" => "Fyodor Dostoevsky",
-            "releaseYear" => 1880,
-            "purchaseUrl" => "https://www.amazon.com/Karamazov-Brothers-Fyodor-Dostoevsky/dp/014018640X"
-        ],
-        [
-            "name" => "The Brothers Karamazov",
-            "author" => "Fyodor Dostoevsky",
-            "releaseYear" => 1880,
-            "purchaseUrl" => "https://www.amazon.com/Brothers-Karamazov-Fyodor-Dostoevsky/dp/014018640X"
-        ],
-        [
-            "name" => "The Idiot",
-            "author" => "Fyodor Dostoevsky",
-            "releaseYear" => 1869,
-            "purchaseUrl" => "https://www.amazon.com/Idiot-Fyodor-Dostoevsky/dp/014018640X"
-        ],
-    ];
+$heading = "Home";
+
+require "functions.php";
+require "views/index.view.php";
 
 
-    $filteredBooks = array_filter($books, function ($book) {
-        return $book['author'] === "Ernest Hemingway";
-    });
 
-    require "index.view.php";
+
+    // $books= [
+    //     [
+    //         "name" => "The Fountainhead",
+    //         "author" => "Ayn Rand",
+    //         "releaseYear" => 1943,
+    //         "purchaseUrl" => "https://www.amazon.com/The-Fountainhead-Novel-Ayn-Rand/dp/0743273567"
+    //     ],
+    //     [
+    //         "name" => "Atlas Shrugged",
+    //         "author" => "Ayn Rand",
+    //         "releaseYear" => 1957,
+    //         "purchaseUrl" => "https://www.amazon.com/Atlas-Shrugged-Novel-Ayn-Rand/dp/045116392X"
+    //     ],
+    //     [
+    //         "name" => "The Virtue of Selfishness",
+    //         "author" => "Ayn Rand",
+    //         "releaseYear" => 1961,
+    //         "purchaseUrl" => "https://www.amazon.com/Virtue-Selfishness-Essays-Ayn-Rand/dp/045116392X"
+    //     ],
+    //     [
+    //         "name" => "Farewell to Arms",
+    //         "author" => "Ernest Hemingway",
+    //         "releaseYear" => 1929,
+    //         "purchaseUrl" => "https://www.amazon.com/Farewell-Arms-Novel-Ernest-Hemingway/dp/0684830195"
+    //     ],
+    //     [
+    //         "name" => "The Old Man and the Sea",
+    //         "author" => "Ernest Hemingway",
+    //         "releaseYear" => 1952,
+    //         "purchaseUrl" => "https://www.amazon.com/Old-Man-Sea-Classic-Literature/dp/0684801063"
+    //     ],
+    //     [
+    //         "name" => "For Whom the Bell Tolls",
+    //         "author" => "Ernest Hemingway",
+    //         "releaseYear" => 1940,
+    //         "purchaseUrl" => "https://www.amazon.com/For-Whom-Bell-Tolls-Hemingway/dp/0684830195"
+    //     ],
+    //     [
+    //         "name" => "Karamazov Brothers",
+    //         "author" => "Fyodor Dostoevsky",
+    //         "releaseYear" => 1880,
+    //         "purchaseUrl" => "https://www.amazon.com/Karamazov-Brothers-Fyodor-Dostoevsky/dp/014018640X"
+    //     ],
+    //     [
+    //         "name" => "The Brothers Karamazov",
+    //         "author" => "Fyodor Dostoevsky",
+    //         "releaseYear" => 1880,
+    //         "purchaseUrl" => "https://www.amazon.com/Brothers-Karamazov-Fyodor-Dostoevsky/dp/014018640X"
+    //     ],
+    //     [
+    //         "name" => "The Idiot",
+    //         "author" => "Fyodor Dostoevsky",
+    //         "releaseYear" => 1869,
+    //         "purchaseUrl" => "https://www.amazon.com/Idiot-Fyodor-Dostoevsky/dp/014018640X"
+    //     ],
+    // ];
+
+
+    // $filteredBooks = array_filter($books, function ($book) {
+    //     return $book['author'] === "Ernest Hemingway";
+    // });
+
