3a9d18f2934e06732f0ca8c5953b10fcc32dae5f
celikerde <celikerde@itu.edu.tr>
2025-07-13 16:36:05 +0300

    Add notes functionality with new routes and views
    - Introduced '/notes' and '/note' routes in router.php to handle notes display and individual note retrieval.
    - Created notes.php controller to fetch and display all notes for a user.
    - Added note.php controller to retrieve and display a specific note based on its ID.
    - Developed views for displaying the list of notes and individual note details.
    - Updated navigation to include a link to the notes page.

---
+++ controllers/note.php
@@ -0,0 +1,16 @@
+<?php
+
+$config = require "config.php";
+
+$db = new Database($config['database']);
+
+$heading = "My Note";
+
+$id = $_GET['id'];
+
+$note = $db->query("SELECT * FROM notes where id = :id", [':id' => $id])->fetch();
+
+
+
+require "views/note.view.php";
+
