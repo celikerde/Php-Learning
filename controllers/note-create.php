773f3a03aaef3ee232ec2113417d32d5af68fe6c
celikerde <celikerde@itu.edu.tr>
2025-07-26 22:25:42 +0300

    fix Implement note insertion in the note creation controller
    - Added database interaction to insert a new note into the database upon form submission.

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -2,9 +2,17 @@
 
 $heading = "Create Note";
 
+$config = require "config.php";
+
+$db = new Database($config['database']);
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $body = $_POST['body'];
-    dd($body);
+    $db->query('INSERT INTO notes (body, user_id) VALUES (:body, :user_id)', [
+        'body' => $body,
+        'user_id' => 2
+    ]);
+
 }
 
 require "views/note-create.view.php";
