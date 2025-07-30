7ebd730313fb15453ec260e654ddf9b9c9d11d25
celikerde <celikerde@itu.edu.tr>
2025-07-30 12:01:58 +0300

    feat: Implement core classes for database management, routing, and response handling
    - Added Database class for managing database connections and queries.
    - Introduced functions for routing and aborting requests in router.php.
    - Created Response class to define standard HTTP response codes.
    - Implemented utility functions in functions.php for debugging and view rendering.
    - Added Validator class for input validation, including string and email checks.

---
+++ Core/router.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+function routeToController($uri, $routes) {
+    if (array_key_exists($uri, $routes)) {
+        require base_path($routes[$uri]);
+    } else {
+        abort(404);
+    }
+}
+
+function abort($code=404) {
+    http_response_code($code);
+    require view("{$code}.php");
+    die();
+}
+
+$routes = require base_path('routes.php');
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+routeToController($uri, $routes);
