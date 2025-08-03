f84e0e5c663385eb8c6b28baac66480621792aa9
celikerde <celikerde@itu.edu.tr>
2025-08-03 19:01:05 +0300

    feat: Refactor note creation logic into a dedicated store controller
    - Moved note creation logic from create.php to a new store.php controller for better separation of concerns.
    - Implemented error handling for note body validation and streamlined the insertion process into the database.
    - Updated the view rendering to handle errors appropriately, improving user experience during note creation.

--- controllers/notes/create.php
+++ controllers/notes/create.php
@@ -1,36 +1,5 @@
 <?php
 
-use Core\Validator;
-use Core\Database;
-
-require base_path("Core/Validator.php");
-
-$config = require base_path("config.php");
-
-$db = new Database($config['database']);
-
-$errors = [];
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $body = $_POST['body'];
-    
-
-    if (!Validator::string($body, 1, 1000)) {
-        $errors['body'] = 'A body is required and must be less than 1000 characters';
-    }
-
-    if (empty($errors)) {
-
-    $db->query('INSERT INTO notes (body, user_id) VALUES (:body, :user_id)', [
-        'body' => $body,
-        'user_id' => 2
-        ]);
-
-        exit();
-    }
-}
-
 view("notes/create.view.php", [
     "heading" => "Create Note",
-    "errors" => $errors,
 ]);
