6c874ecf30781302e165034117f5e2f2d9b4959d
celikerde <celikerde@itu.edu.tr>
2025-08-03 19:00:30 +0300

    feat: Enhance note routing with additional HTTP methods
    - Added DELETE, POST, PATCH, and PUT methods for note management in routes.php.
    - Updated routing to support note creation, deletion, and updates, improving the overall functionality of the notes feature.

--- routes.php
+++ routes.php
@@ -6,7 +6,11 @@ $router->get('/contact', 'controllers/contact.php');
 
 $router->get('/notes', 'controllers/notes/index.php');
 $router->get('/note', 'controllers/notes/show.php');
-$router->get('/notes/create', 'controllers/notes/create.php');
+$router->delete('/note', 'controllers/notes/destroy.php');
 
-$router->delete('/notes', 'controllers/notes/destroy.php');
+$router->get('/notes/create', 'controllers/notes/create.php');
+$router->post('/notes', 'controllers/notes/store.php');
+
+$router->patch('/note', 'controllers/notes/update.php');
+$router->put('/note', 'controllers/notes/update.php');
 
