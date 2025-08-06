f70e3dff79eb7f0acf98782c32667aaf3562fbf7
celikerde <celikerde@itu.edu.tr>
2025-08-06 12:29:55 +0300

    feat: Add registration functionality with view and route
    - Introduced a new route for user registration, linking to the registration controller.
    - Created a registration controller to render the registration view with a heading.
    - Added a registration view that includes the necessary HTML structure and partials for a complete registration page.

---
+++ views/registration/create.view.php
@@ -0,0 +1,11 @@
+<?php view("partials/head.php"); ?>
+<?php view("partials/nav.php"); ?>
+<?php view("partials/banner.php"); ?>
+
+<main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <h1>Register Here</h1>
+    </div>
+</main>
+
+<?php require view("partials/footer.php"); ?>
