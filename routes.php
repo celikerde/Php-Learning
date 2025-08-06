08a16808bdcfc946be5cc927d7fbba708e11b247
celikerde <celikerde@itu.edu.tr>
2025-08-06 11:14:01 +0300

    refactor: Update note routes for improved editing functionality
    - Retained the '/notes/create' and '/notes' routes for creating and storing notes, ensuring consistent functionality.
    - This change enhances the clarity of the routing structure for note management.

--- routes.php
+++ routes.php
@@ -8,9 +8,8 @@ $router->get('/notes', 'controllers/notes/index.php');
 $router->get('/note', 'controllers/notes/show.php');
 $router->delete('/note', 'controllers/notes/destroy.php');
 
+$router->get('/note/edit', 'controllers/notes/edit.php');
+$router->patch('/note', 'controllers/notes/update.php');
+
 $router->get('/notes/create', 'controllers/notes/create.php');
 $router->post('/notes', 'controllers/notes/store.php');
-
-$router->patch('/note', 'controllers/notes/update.php');
-$router->put('/note', 'controllers/notes/update.php');
-
