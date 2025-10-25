a17a02c594de576cbd4ea410da179c49eabdd6f5
celikerde <erdem@unusualgrowth.com>
2025-10-25 16:49:59 +0300

    Revert "feat: Enhance routing with error handling and session management"
    This reverts commit 3a0ecff765d3543f2037f5b5fdbaddc0037e9e72.

--- public/index.php
+++ public/index.php
@@ -1,15 +1,17 @@
 <?php
 
-use Core\Session;
-use Core\ValidationException;
-
 session_start();
 
 const BASE_PATH = __DIR__ . '/../';
 
-require BASE_PATH . "vendor/autoload.php";
 
 require BASE_PATH . "Core/functions.php";
+
+spl_autoload_register(function ($class) {
+    // dd($class);
+    $class = str_replace('\\',DIRECTORY_SEPARATOR,$class);
+    require base_path("{$class}.php");
+});
 
 require base_path('bootstrap.php');
 
@@ -20,14 +22,8 @@ $routes = require base_path('routes.php');
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
-
-try {
-    $router->route($uri, $method);
-} catch (ValidationException $e) {
-    Session::flash('errors', $e->errors);
-    Session::flash('old', $e->old);
-    return redirect($router->previousUrl());
-}
+// dd($method);
+$router->route($uri, $method);
 
 
 
