54a9b318b1d5728ceea6dae814fbd2a9091ac6f8
celikerde <erdem@unusualgrowth.com>
2025-10-25 16:43:26 +0300

    feat: Implement ValidationException and enhance LoginForm validation
    - Added a new ValidationException class to handle validation errors more effectively.
    - Refactored LoginForm to utilize the new ValidationException for error handling during validation.
    - Updated session store controller to streamline login process and error reporting.

--- Http/controllers/session/store.php
+++ Http/controllers/session/store.php
@@ -1,31 +1,22 @@
 <?php
 
-use Core\Validator;
-use Core\Database;
-use Core\App;
-use Http\Forms\LoginForm;
 use Core\Authenticator;
-use Core\Session;
+use Http\Forms\LoginForm;
 
-$db = App::resolve(Database::class);
-
-$email = $_POST['email'];
-$password = $_POST['password'];
-
-$form = new LoginForm();
-
-if($form->validate($email, $password)) {
-
-    if((new Authenticator)->attempt($email, $password)) {
-        redirect('/');
-    }
-        $form->error('password', 'No matching account found for that email address and password');
-}
-
-Session::flash('errors', $form->errors());
-Session::flash('old', [
-    'email' => $email
+$form = LoginForm::validate($attributes = [
+    'email' => $_POST['email'],
+    'password' => $_POST['password'],
 ]);
 
+$signedIn = (new Authenticator)->attempt(
+    $attributes['email'], $attributes['password']
+);
+    
 
-return redirect('/login');
+if(!$signedIn) {
+    $form->error(
+        'email', 'No matching account found for that email address and password.'
+    )->throw();
+}
+
+redirect('/');
